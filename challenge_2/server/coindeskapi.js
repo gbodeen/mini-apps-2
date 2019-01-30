2460ea9225cc7da258d7d7ece355ca8d590eacd8
Gabriel Bodeen <gabebodeen@gmail.com>
2019-01-30 14:39:52 -0600

    Add server call to CoinDesk API to the route.

--- challenge_2/server/coindeskapi.js
+++ challenge_2/server/coindeskapi.js
@@ -4,9 +4,14 @@
 // ?index=[USD/CNY]The index to return data for. Defaults to USD.
 // ?currency=<VALUE>The currency to return the data in, specified in ISO 4217 format. Defaults to USD.
 // ?start=<VALUE>&end=<VALUE> Allows data to be returned for a specific date range. Must be listed as a pair of start and end parameters, with dates supplied in the YYYY-MM-DD format, e.g. 2013-09-01 for September 1st, 2013.
+const axios = require('axios');
+
 
 const getBTCinfo = (startDate, endDate, callback) => {
-
+  let url = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+  axios.get(url)
+    .then(response => callback(null, response))
+    .catch(err => callback(err));
 }
 
 module.exports = getBTCinfo;
